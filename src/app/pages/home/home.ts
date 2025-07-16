34593ca798ba9418a5cdd72a5e758a18b0bde8bc
jrbistuer <jrbistuer@gmail.com>
2025-07-16 08:20:43 +0200

    formularios

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from '../../components/header/header';
+import { IBook } from '../../models/interfaces';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,18 @@ import { Header } from '../../components/header/header';
 export class Home {
 
   title: string = 'Home';
+  llistaLlibres: any[] = [];
 
   constructor() {
     // Initialization logic can go here if needed
+    const llibre1: IBook = {
+      id: 1,
+      titol: 'El Quixot',
+      autor: 'Miguel de Cervantes',
+      anyPublicacio: 1605,
+      editorial: 'Editorial Clásicos',
+      pagines: 1023
+    };
   }
 
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from "../../components/header/header";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule, Header],
+  imports: [RouterModule, Header, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
@@ -12,8 +14,42 @@ export class Login {
 
   title: string = 'Login';
 
+  loginForm!: FormGroup; // Define the type of loginForm if using Reactive Forms
+
   constructor() {
     // Initialization logic can go here if needed
-  } 
+    this.createForm();
+  }
+
+  enviar() {
+    // Logic to handle form submission can be added here
+    console.log('Form submitted');
+    const usuari = (document.getElementById('username') as HTMLInputElement).value;
+    const contrasenya = (document.getElementById('password') as HTMLInputElement).value;
+    console.log(`Usuari: ${usuari}, Contrasenya: ${contrasenya}`);
+    // You can add further logic to handle the login process
+  }
+
+  enviarReactiveForm() {
+    // Logic to handle form submission with Reactive Forms
+    console.log('Reactive Form submitted', this.loginForm);
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log(`Username: ${username}, Password: ${password}`);
+      // You can add further logic to handle the login process
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  createForm() {
+    // Logic to create a form can be added here if needed
+    console.log('Creating form');
+    // Creating Reactive Form
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
 
 }
