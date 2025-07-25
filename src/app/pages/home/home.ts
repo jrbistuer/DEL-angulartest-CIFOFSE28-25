d25954b013851ab77bd90c1acbfa10701f8db3e3
jrbistuer <jrbistuer@gmail.com>
2025-07-25 17:29:47 +0200

    form llibres component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,15 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from '../../components/header/header';
 import { IBook } from '../../models/interfaces';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BooksService } from '../../services/books';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { FormLlibres } from '../../components/form-llibres/form-llibres';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, Header, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterModule, Header, FormLlibres],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
@@ -19,43 +16,18 @@ export class Home {
   title: string = 'Home';
   llistaLlibres: IBook[] = [];
 
-  index = 0;
-
-  bookForm!: FormGroup; // Define the type of bookForm if using Reactive Forms
+  index = -1;
 
   constructor(private booksService: BooksService) {
     this.llistaLlibres = this.booksService.getBooks();
-    this.createBookForm();
   }
 
-  createBookForm(): void {
-    this.bookForm = new FormGroup({
-      titol: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      autor: new FormControl('', Validators.required),
-      anyPublicacio: new FormControl(''),
-      editorial: new FormControl('', Validators.required),
-      pagines: new FormControl('', Validators.required),
-      isbn: new FormControl('')
-    });
+  submitBook(newBook: IBook): void {
+    console.log('New book que rebem del component fill:', newBook);
+    this.llistaLlibres.push(newBook);
+    this.booksService.saveBooks(this.llistaLlibres);
   }
 
-  submitBook(): void {
-    if (this.bookForm.valid) {
-      const newBook: IBook = this.bookForm.value;
-      this.llistaLlibres.push(newBook);
-      this.booksService.saveBooks(this.llistaLlibres);
-      this.bookForm.reset();
-    }
-  }
-
-  submitEditBook() {
-    if (this.bookForm.valid) {
-      const newBook: IBook = this.bookForm.value;
-      this.llistaLlibres[this.index] = newBook;
-      this.booksService.saveBooks(this.llistaLlibres);
-      this.bookForm.reset();
-    }
-  }
 
   deleteLlibre(index: number): void {
     this.booksService.deleteBook(index);
@@ -64,16 +36,13 @@ export class Home {
 
   editLlibre(index: number): void {
     this.index = index;
+  }
 
-    const bookToEdit = this.llistaLlibres[index];
-    this.bookForm.setValue({
-      titol: bookToEdit.titol,
-      autor: bookToEdit.autor,
-      anyPublicacio: bookToEdit.anyPublicacio || '',
-      editorial: bookToEdit.editorial,
-      pagines: bookToEdit.pagines,
-      isbn: bookToEdit.isbn || ''
-    });
+  submitEditBook(newBook: IBook): void {
+    console.log('New Book al component Home:', newBook);
+    this.llistaLlibres[this.index] = newBook;
+    this.booksService.saveBooks(this.llistaLlibres);
+    this.index = -1; // Reset index after editing
   }
 
 }
